43ae67182ff3cbf6e572d1102005661a2dfe70b3
yapel <elmaildejapel@gmail.com>
2016-04-21 18:16:30 -0300

    Added admin interface

--- web/app.py
+++ web/app.py
@@ -10,18 +10,24 @@ from flask.ext.mongoengine import MongoEngine
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.mongoengine import ModelView
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.security import Security, MongoEngineUserDatastore, \
+    UserMixin, RoleMixin, login_required, utils, current_user
+
 from flask.ext import admin, login
 from flask.ext.admin import helpers
 
 import flask_admin as flask_admin
 
 from flask_debugtoolbar import DebugToolbarExtension
-
+from flask_security import utils
 
 from models import Contact, Message, User, SendLog, ReceiveLog, \
     MyModelView, MyAdminIndexView, UserView, \
     ContactView, MessageView, \
     LoginForm, RegistrationForm
+
+from wtforms.fields import PasswordField
+
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -35,9 +41,110 @@ toolbar = DebugToolbarExtension(app)
 # Create models
 db = MongoEngine(app)
 
+_redis = redis.StrictRedis(host='redis', port=6379)
+
+# ADMIN #
+
+
+class Role(db.Document, RoleMixin):
+    name = db.StringField(max_length=80, unique=True)
+    description = db.StringField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    # __hash__ is required to avoid the exception TypeError: unhashable type: 'Role' when saving a User
+    def __hash__(self):
+        return hash(self.name)
+
+
+class User(db.Document, UserMixin):
+    email = db.StringField(max_length=255)
+    password = db.StringField(max_length=255)
+    active = db.BooleanField(default=True)
+    confirmed_at = db.DateTimeField()
+    roles = db.ListField(db.ReferenceField(Role), default=[])
+
+# Setup Flask-Security
+user_datastore = MongoEngineUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    # Create the Roles "admin" and "end-user" -- unless they already exist
+    user_datastore.find_or_create_role(name='admin', description='Administrator')
+
+    # Create two Users for testing purposes -- unless they already exists.
+    # In each case, use Flask-Security utility function to encrypt the password.
+    encrypted_password = utils.encrypt_password('que sabroso el caramel en el delta123')
+    if not user_datastore.get_user('dropkek@oneline.net'):
+        user_datastore.create_user(email='dropkek@oneline.net', password=encrypted_password)
+
+# Views
+@app.route('/admin')
+@login_required
+def home():
+    return render_template('admin.html')
+
+
+# Customized User model for SQL-Admin
+class UserAdmin(ModelView):
+
+    # Don't display the password on the list of Users
+    column_exclude_list = ('password',)
+
+    # Don't include the standard password field when creating or editing a User (but see below)
+    form_excluded_columns = ('password',)
+
+    # Automatically display human-readable names for the current and available Roles when creating or editing a User
+    column_auto_select_related = True
+
+    # Prevent administration of Users unless the currently logged-in user has the "admin" role
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    # On the form for creating or editing a User, don't display a field corresponding to the model's password field.
+    # There are two reasons for this. First, we want to encrypt the password before storing in the database. Second,
+    # we want to use a password field (with the input masked) rather than a regular text field.
+    def scaffold_form(self):
+
+        # Start with the standard form as provided by Flask-Admin. We've already told Flask-Admin to exclude the
+        # password field from this form.
+        form_class = super(UserAdmin, self).scaffold_form()
+
+        # Add a password field, naming it "password2" and labeling it "New Password".
+        form_class.password2 = PasswordField('New Password')
+        return form_class
+
+    # This callback executes when the user saves changes to a newly-created or edited User -- before the changes are
+    # committed to the database.
+    def on_model_change(self, form, model, is_created):
+
+        # If the password field isn't blank...
+        if len(model.password2):
+
+            # ... then encrypt the new password prior to storing it in the database. If the password field is blank,
+            # the existing password in the database will be retained.
+            model.password = utils.encrypt_password(model.password2)
+
+
+# Customized Role model for SQL-Admin
+class RoleAdmin(ModelView):
+
+    # Prevent administration of Roles unless the currently logged-in user has the "admin" role
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+# Initialize Flask-Admin
 admin = Admin(app)
 
-_redis = redis.StrictRedis(host='redis', port=6379)
+# Add Flask-Admin views for Users and Roles
+admin.add_view(UserAdmin(User))
+admin.add_view(RoleAdmin(Role))
+# ADMIN #
+
 
 # Initialize flask-login
 def init_login():
@@ -141,7 +248,7 @@ def logout_view():
     login.logout_user()
     return redirect(url_for('index'))
 
-admin.add_view(UserView(User))
+#admin.add_view(UserView(User))
 admin.add_view(ContactView(Contact))
 admin.add_view(MessageView(Message))
 admin.add_view(ModelView(SendLog))
